51e473ccbaa3a8f986c4cfe155550b4011d2e4b5
AmberTJC <147658220+AmberTJC@users.noreply.github.com>
2025-04-04 21:35:21 -0600

    javadoc for DLL and Queue

--- src/implementations/MyDLL.java
+++ src/implementations/MyDLL.java
@@ -1,3 +1,8 @@
+/**
+ * @author amber c, nawal m, aiana s
+ */
+
+
 package implementations;
 
 import java.io.Serializable;
@@ -5,7 +10,10 @@ import java.util.NoSuchElementException;
 
 import utilities.Iterator;
 import utilities.ListADT;
-
+/**
+ * Using the ListADT MyDLL provides a data structure to hold elements in a list.
+ * @param <E> The elements to be held by the list
+ */
 public class MyDLL<E> implements ListADT<E>, Serializable
 {
 	
@@ -19,13 +27,22 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 		tail = null;
 		size = 0;
 	}
-
+/**
+ * Size method returns the number of elements in the list
+ * Precondition: list exists
+ * Postcondition: number of elements returned
+ */
 	@Override
 	public int size()
 	{
 		return size;
 	}
 
+	/**
+	 * clears the elements from the list
+	 * Precondition: list exists
+	 * Postcondition: list is cleared
+	 */
 	@Override
 	public void clear()
 	{
@@ -35,6 +52,16 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 		
 	}
 
+	
+	/**
+	 * adds an element to the list
+	 * Precondition: List exists and value not null or invalid index
+	 * Postcondition: value added to the list
+	 * @param index the index of the value to be added
+	 * @param toAdd the value to be added
+	 * @exception throws NullPointerException when value to be added is null and IndexOutOfBoundsException when the
+	 * index is not a permissible value
+	 */
 	@Override
 	public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException
 	{
@@ -84,7 +111,13 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 		size++;
 		return true;
 	}
-
+/**
+ * Adds an element to the list
+ * Precondition: List exists
+ * Postcondition: value added to list
+ * @param toAdd the element to be added
+ * @exception throws NullPointerException when value to be added is null
+ */
 	@Override
 	public boolean add(E toAdd) throws NullPointerException
 	{
@@ -110,7 +143,14 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 	}
 
 	
-	
+	/**
+	 * Adds all values to list
+	 * Precondition: List exists
+	 * Postcondition: All values added to list
+	 * @param toAdd values to be added
+	 * @exception throws NullPointerException when values to be added are null
+	 * 
+	 */
 	@Override
 	public boolean addAll(ListADT<? extends E> toAdd) throws NullPointerException
 	{
@@ -124,7 +164,13 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 		}
 		return true;
 	}
-
+/**
+ * gets the value of a given node
+ * Precondition: List exists
+ * Postcondition: value retrieved
+ * @param index the index of the node to be retrieved
+ * @exception throws IndexOutOfBoundsException when index is not permissible 
+ */
 	@Override
 	public E get(int index) throws IndexOutOfBoundsException
 	{
@@ -134,7 +180,13 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 		}
 		return getNode(index).getElement();
 	}
-
+/**
+ * removes a node from the list by index
+ * Precondition: List exists
+ * Postcondition: node is removed
+ * @param index the index of the node to be removed
+ * @exception throws IndexOutOfBoundsException when the index is not permissible.
+ */
 	@Override
 	public E remove(int index) throws IndexOutOfBoundsException
 	{
@@ -147,7 +199,13 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 		
 		return unlink(node);
 	}
-
+/**
+ * removes a node by element
+ * Precondition: list exists and node is not null
+ * Postcondition: node is removed
+ * @param toRemove the element to be removed
+ * @exception throws NullPointerException if the element to be removed is null
+ */
 	@Override
 	public E remove(E toRemove) throws NullPointerException
 	{
@@ -167,7 +225,15 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 		}
 		return null;
 	}
-
+/**
+ * sets the element of a given node
+ * Precondition: list exists
+ * Postcondition: element of a node is changed
+ * @param index the index of the node to be changed
+ * @param toChange the element to be changed
+ * @exception throws NullPointerException if value is null, IndexOutOfBoundsException if the index is not
+ * permissible
+ */
 	@Override
 	public E set(int index, E toChange) throws NullPointerException, IndexOutOfBoundsException
 	{
@@ -186,13 +252,24 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 		
 		return old;
 	}
-
+/**
+ * checks whether the list is empty returns true if empty false otherwise
+ * Precondition: list exists
+ * Postcondition: nothing changes and boolean value is returned
+ */
 	@Override
 	public boolean isEmpty()
 	{
 		return size == 0;
 	}
 
+	/**
+	 * searches list for a given value
+	 * Precondition: list exists
+	 * Postcondition: returns true if value found false otherwise
+	 * @param toFind the value to be found
+	 * @exception throws NullPointerException if value is null
+	 */
 	@Override
 	public boolean contains(E toFind) throws NullPointerException
 	{
@@ -213,6 +290,13 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 		return false;
 	}
 
+	/**
+	 * Puts values into an array
+	 * Precondition: List exists
+	 * Postcondition: array created with the values of the list
+	 * @param toHold the array created
+	 * @exception throws NullPointerException if values are null
+	 */
 	@Override
 	public E[] toArray(E[] toHold) throws NullPointerException
 	{
@@ -239,6 +323,11 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 		return toHold;
 	}
 
+	/**
+	 * Puts values into an array
+	 * Precondition: List exists
+	 * Postcondition: array created with the values of the list
+	 */
 	@Override
 	public Object[] toArray()
 	{
@@ -254,6 +343,11 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 		return result;
 	}
 
+	/**
+	 * Iterator to iterate through the values of the list
+	 * Precondition: list exists
+	 * Postcondition: List iterated through
+	 */
 	@Override
 	public Iterator<E> iterator()
 	{
@@ -262,6 +356,14 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 	
 	
 	//Helper functions
+	/**
+	 * retrieves a given node
+	 * Precondition: list exists
+	 * Postcondition: node is retrieved
+	 * @param index the index of the node to be retrieved
+	 * @return the node specified to be retrieved
+	 * @throws IndexOutOfBoundsException when the index is not permissible
+	 */
 	private MyDLLNode<E> getNode(int index) throws IndexOutOfBoundsException
 	{
 		if (index <0 || index >= size) 
@@ -290,6 +392,13 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 		return current;
 	}
 	
+	/**
+	 * unlinks a node from the list
+	 * Precondition: list exists
+	 * Postcondition: node is unlinked
+	 * @param node the node to be unlinked
+	 * @return the unlinked node
+	 */
 	private E unlink (MyDLLNode<E> node) 
 	{
 		E data = node.getElement();
@@ -316,17 +425,31 @@ public class MyDLL<E> implements ListADT<E>, Serializable
 		return data;
 	}
 
-	
+	/**
+	 * the custom Iterator for the MyDLL class
+	 * Precondition: list exists
+	 * Postcondition: List iterated through
+	 */
 		private class MyDLLIterator implements Iterator<E>
 		{
 			private MyDLLNode<E> current = head;
 			
+			/**
+			 * Determines if there is another value next in the list
+			 * Precondition: list exists
+			 * Postcondition: returns the value if it is not null
+			 */
 			@Override
 			public boolean hasNext()
 			{
 				return current !=null;
 			}
 
+			/**
+			 * gets the next element in the list
+			 * Precondition: list exists
+			 * Post condition: next element is retrieved
+			 */
 			@Override
 			public E next() throws NoSuchElementException
 			{
--- src/implementations/MyQueue.java
+++ src/implementations/MyQueue.java
@@ -1,3 +1,8 @@
+/**
+ * @author amber c, nawal m, aiana s
+ */
+
+
 package implementations;
 
 import java.util.NoSuchElementException;
@@ -6,6 +11,11 @@ import exceptions.EmptyQueueException;
 import utilities.Iterator;
 import utilities.QueueADT;
 
+
+/**
+ * Provides standard queue operations
+ * @param <E> elements in the queue
+ */
 public class MyQueue<E> implements QueueADT<E>
 {
 	
@@ -25,16 +35,29 @@ public class MyQueue<E> implements QueueADT<E>
 		}
 	}
 	
+	/**
+	 * A constructor for MyQueue that tracks the queues capacity
+	 * @param capacity how many elements the queue can hold
+	 */
 	public MyQueue(int capacity) {
 	    if (capacity <= 0) //>=
 	    throw new IllegalArgumentException("Queue capacity must be greater than zero");
 		this.capacity = capacity;
 	}
-
+/**
+ * A constructor for MyQueue
+ */
 	public MyQueue()
 	{ this.capacity = Integer.MAX_VALUE;
 	}
 
+	/**
+	 * adds an element to the back of the queue
+	 * Precondition: value cannot be null
+	 * Postcondition: element added to back of the queue
+	 * @param toAdd the element to be added
+	 * @exception throws NullPointerException when value is null, IllegalStateException when queue is full
+	 */
 	@Override
 	public void enqueue(E toAdd) throws NullPointerException
 	{
@@ -62,6 +85,12 @@ public class MyQueue<E> implements QueueADT<E>
 		size++;
 	}
 
+	/**
+	 * removes an element from the front of the queue
+	 * Precondition: queue cannot be empty
+	 * Postcondition: element is removed from the queue
+	 * @exception throws EmptyQueueException when the queue is empty
+	 */
 	@Override
 	public E dequeue() throws EmptyQueueException
 	{
@@ -80,6 +109,11 @@ public class MyQueue<E> implements QueueADT<E>
 		return data;
 	}
 
+	/**
+	 * shows front value without removing it from the queue
+	 * Precondition: value cannot be null, queue cannot be empty
+	 * Postcondition: value a front is shown but not removed
+	 */
 	@Override
 	public E peek() throws EmptyQueueException
 	{
@@ -90,6 +124,11 @@ public class MyQueue<E> implements QueueADT<E>
 		return front.data;
 	}
 
+	/**
+	 * removes all values from queue
+	 * Precondition: queue exists
+	 * Postcondition: queue is empty
+	 */
 	@Override
 	public void dequeueAll()
 	{
@@ -99,12 +138,24 @@ public class MyQueue<E> implements QueueADT<E>
 		
 	}
 
+	/**
+	 * Checks whether the queue is empty, returns true if it is false otherwise
+	 * Precondition: Queue exists
+	 * Postcondition: returns true if empty
+	 */
 	@Override
 	public boolean isEmpty()
 	{
 		return size == 0;
 	}
 
+	/**
+	 * checks if a given value exists in the queue
+	 * Preconditions: queue exists
+	 * Postconditions: return true if found false otherwise
+	 * @param toFind the value to be searched
+	 * @exception throws NullPointerException if value is null
+	 */
 	@Override
 	public boolean contains(E toFind) throws NullPointerException
 	{
@@ -124,7 +175,12 @@ public class MyQueue<E> implements QueueADT<E>
 		}
 		return false;
 	}
-
+	/**
+	 *searches for a given value within the queue
+	 * Preconditions: queue exists
+	 * Postconditions: position of the searched value
+	 * @param toFind the value to be searched
+	 */
 	@Override
 	public int search(E toFind)
 	{
@@ -143,22 +199,43 @@ public class MyQueue<E> implements QueueADT<E>
 		return -1;
 	}
 
+/**
+ * Custom Iterator for the MyQueue class
+ * Precondition: queue exists
+ * Postcondition: queue iterated through
+ */
 	@Override
 	public Iterator<E> iterator()
 	{
 		return new QueueIterator();
 	}
 	
+	/**
+	 * Custom Iterator for the MyQueue class
+	 * Precondition: queue exists
+	 * Postcondition: queue iterated through
+	 */
 	private class QueueIterator implements Iterator<E>
 	{
 		private Node<E> current = front;
 
+		/**
+		 * Determines if any value in the queue has a next value
+		 * Precondition: queue exists
+		 * Postconditions: return true if value has a next value false otherwise
+		 */
 		@Override
 		public boolean hasNext()
 		{
 			return current != null;
 		}
 
+		/**
+		 * determines what the next value is in the queue
+		 * Precondition: queue exists
+		 * Postcondition: returns next value
+		 * @exception NoSuchElementException throws when there is no element next
+		 */
 		@Override
 		public E next() throws NoSuchElementException
 		{
@@ -175,6 +252,12 @@ public class MyQueue<E> implements QueueADT<E>
 		
 	}
 
+	/**
+	 * compares two queues
+	 * Preconditions: Queues must contain equal items appearing in the same order
+	 * Postconditions: reruns true if both queues are equal, false otherwise
+	 * @param that the queue to be compared
+	 */
 	@Override
 	public boolean equals(QueueADT<E> that)
 	{
@@ -197,6 +280,11 @@ public class MyQueue<E> implements QueueADT<E>
 		return !thisIter.hasNext() && !thatIter.hasNext();
 	}
 
+	/**
+	 * returns an array of all items in the queue in sequence
+	 * Preconditions: Queue must exist
+	 * Postconditions: array is returned
+	 */
 	@Override
 	public Object[] toArray()
 	{
@@ -212,6 +300,13 @@ public class MyQueue<E> implements QueueADT<E>
 		return result;
 	}
 
+	/**
+	 * returns an array of all items in the queue in sequence
+	 * Preconditions: Queue must exist
+	 * Postconditions: array is returned
+	 * @param holder the array to be returned
+	 * @exception throws NullPointerException when the value is null
+	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public E[] toArray(E[] holder) throws NullPointerException
@@ -242,12 +337,22 @@ public class MyQueue<E> implements QueueADT<E>
 		return holder;
 	}
 
+	/**
+	 * returns true if queue is full
+	 * Precondition: queue exists
+	 * Postcondition: returns true if queue is full
+	 */
 	@Override
 	public boolean isFull()
 	{
 		return size == capacity;
 	}
 
+	/**
+	 * shows how many elements are in the queue
+	 * Preconditions: queue exists
+	 * Postconditions: return the number of elements in the queue
+	 */
 	@Override
 	public int size()
 	{
